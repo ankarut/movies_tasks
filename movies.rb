734e93633a6070e3cfd91e897240a583eefde54e
ankarut <ankaroot@gmail.com>
2016-09-21 01:17:54 +0300

    Convert to array of hashes.

--- movies.rb
+++ movies.rb
@@ -1,39 +1,37 @@
-file_name = ARGV[0] ? ARGV[0] : 'movies.txt'
+MAX_RATING = 10
+FIELDS = %i{ Link Title Year Country PremierDate Genre Duration Rating Director Stars}
+DEFAULT_FILE = 'movies.txt'
 
-puts "File with name '#{file_name}' is not exists." unless File.exist?(file_name)
-
-# 1
-file = File.open(file_name, 'r')
-movies = file.readlines.map { |line| line.strip.split('|') }
-
-# 2
-
-FIELDS = {
-  Title: 1,
-  Rating: 7
-}
-
-def search_movies_by_title(title, movies)
-  movies.select { |movie| movie[1][/#{title}/i] }
+def search_movies_by_title(title)
+  @movies.select { |movie| movie[:Title][/#{title}/i] }
 end
 
-def get_field(field, movie)
-  movie[FIELDS[field.to_sym]]
-end
-
-search_result = search_movies_by_title('Time', movies)
-puts search_result.map { |movie| "#{get_field(:Title, movie)}, #{get_field(:Rating, movie)}" }
-
-# 3
-def rating_asteriscs(rating)
-  _, @points = rating.split('.').map &:to_i
+def rating_asteriscs(movie)
+  _, @points = movie[:Rating].split('.').map &:to_i
   asteriscs = '*' * @points
 end
 
 def pretty_space_separator
-  ' ' * (10 - @points)
+  ' ' * (MAX_RATING - @points)
 end
 
-puts search_result.map { |movie| [rating_asteriscs(get_field(:Rating, movie)), get_field(:Title, movie)].join(pretty_space_separator) }
+
+# 4-5
+file_name = ARGV[0] ? ARGV[0] : DEFAULT_FILE
+puts "File with name '#{file_name}' is not exists." unless File.exist?(file_name)
+
+# 1
+file = File.open(file_name, 'r')
+@movies = file.readlines.map { |line| Hash[FIELDS.zip(line.strip.split('|'))] }
+
+# 2
+search_result = search_movies_by_title('Time')
+puts search_result.map { |movie| "#{ movie[:Title] }, #{movie[:Rating] }" }
+
+# 3
+puts search_result.map { |movie| [rating_asteriscs(movie), movie[:Title] ].join(pretty_space_separator) }
 
 file.close
+
+
+
