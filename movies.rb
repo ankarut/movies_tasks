e612dc69966c22b5d0c86321298e0a2b92624634
ankarut <ankaroot@gmail.com>
2016-09-23 20:19:14 +0300

    Task 3. Improvements.

--- movies.rb
+++ movies.rb
@@ -22,36 +22,21 @@ search_result.map { |movie| "#{movie[:Title]}, #{movie[:Rating]}" }
 # 3
 search_result.map { |movie| ['*' * rating(movie), movie[:Title]].join(' ' * (MAX_RATING - rating(movie))) }
 
-## Task3 #2
-
-puts @movies.sort_by { |movie| movie[:Duration].to_i }.last(5)
-
-puts @movies.each{ |movie|
-  y, m, d = movie[:PremierDate].split('-')
-
-  y ||= '0000'
-  m ||= '01'
-  d ||= '01'
-
-  movie[:PremierDate] = Date.parse([y, m, d].join('-'))
-}
-
-puts @movies.select { |movie| movie[:Genre][/comedy/i] }.sort_by { |movie| movie[:PremierDate] }.first(5)
-
-directors = @movies.map { |movie| movie[:Director] }.uniq.map do |name|
-  _n = name.split
-  fname = _n.first(_n.count - 1).join(' ')
-  lname = _n.last
-  { lname: lname, fname: fname }
-end
-
-puts directors.sort_by { |n| n[:lname] }.map { |n| n[:fname] + ' ' + n[:lname] }
-
-puts @movies.reject { |movie| movie[:Country][/USA/i] }.count
-
+#--------------------------------------------------------------------------------------------------------------
 ## Task3 #3
 def pretty_print(movies = '')
-  puts movies.map { |movie| "#{movie[:Title]} (#{movie[:PremierDate]}; #{movie[:Genre]}) - #{movie[:Duration]}" }
+  puts movies.map { |movie| "#{movie[:Title]} (#{movie[:PremierDate]}; #{movie[:Genre]}); #{movie[:Director]}) - #{movie[:Duration]}" }
 end
 
 pretty_print @movies
+
+## Task3 #2
+pretty_print @movies.sort_by { |movie| movie[:Duration].to_i }.last(5)
+
+pretty_print @movies.select { |movie| movie[:Genre][/comedy/i] }.sort_by{ |movie|  movie[:PremierDate] }.first(10)
+
+pretty_print @movies.sort_by {  |movie|   movie[:Director].split.last  }
+puts @movies.sort_by {  |movie|   movie[:Director].split.last  }.map{ |movie| movie[:Director] }.uniq
+
+puts @movies.reject { |movie| movie[:Country][/USA/i] }.count
+
