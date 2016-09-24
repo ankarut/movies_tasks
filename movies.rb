ebd6c625d04a4684d4b233dd6c58145a5043d65d
ankarut <ankaroot@gmail.com>
2016-09-24 22:54:37 +0300

    Task 3.

--- movies.rb
+++ movies.rb
@@ -1,42 +1,35 @@
 require 'date'
+require 'ostruct'
+require 'csv'
 
-MAX_RATING = 10
-FIELDS = %i( Link Title Year Country PremierDate Genre Duration Rating Director Stars)
-DEFAULT_FILE = 'movies.txt'
-
-def rating(movie)
-  movie[:Rating].split('.').last.to_i
-end
-
-# 4-5
-file_name = ARGV[0] || DEFAULT_FILE
+file_name = ARGV[0] || 'movies.txt'
 abort("File with name '#{file_name}' is not exists.") unless File.exist?(file_name)
 
-# 1
-@movies = File.readlines(file_name).map { |line| FIELDS.zip(line.strip.split('|')).to_h }
+options = {
+  col_sep: '|',
+  headers: %i( link title year country premierdate genre duration rating director stars)
+}
 
-# 2
-search_result = @movies.select { |movie| movie[:Title][/Time/i] }
-search_result.map { |movie| "#{movie[:Title]}, #{movie[:Rating]}" }
+@movies = CSV.open(file_name, options).to_a.map { |row| OpenStruct.new row.to_h }
 
-# 3
-search_result.map { |movie| ['*' * rating(movie), movie[:Title]].join(' ' * (MAX_RATING - rating(movie))) }
-
-#--------------------------------------------------------------------------------------------------------------
-## Task3 #3
-def pretty_print(movies = '')
-  puts movies.map { |movie| "#{movie[:Title]} (#{movie[:PremierDate]}; #{movie[:Genre]}); #{movie[:Director]}) - #{movie[:Duration]}" }
+MAX_RATING = 10
+def rating(movie)
+  movie.rating.split('.').last.to_i
 end
 
-pretty_print @movies
+puts @movies.select { |movie| movie.title[/Time/i] }
+  .map { |movie| ['*' * rating(movie), movie.title].join(' ' * (MAX_RATING - rating(movie))) }
 
-## Task3 #2
-pretty_print @movies.sort_by { |movie| movie[:Duration].to_i }.last(5)
+def pretty_print(movies = '')
+  puts movies.map { |movie| "#{movie.title} (#{movie.premierdate}; #{movie.genre}; #{movie.director}) - #{movie.duration}" }
+end
 
-pretty_print @movies.select { |movie| movie[:Genre][/comedy/i] }.sort_by{ |movie|  movie[:PremierDate] }.first(10)
+pretty_print @movies.sort_by { |movie| movie.duration.to_i }.last(5)
 
-pretty_print @movies.sort_by {  |movie|   movie[:Director].split.last  }
-puts @movies.sort_by {  |movie|   movie[:Director].split.last  }.map{ |movie| movie[:Director] }.uniq
+pretty_print @movies.select { |movie| movie.genre[/comedy/i] }.sort_by(&:premierdate).first(10)
 
-puts @movies.reject { |movie| movie[:Country][/USA/i] }.count
+puts @movies.sort_by { |movie| movie.director.split.last }.map(&:director).uniq
 
+puts @movies.reject { |movie| movie.country[/USA/i] }.count
+
+puts @movies.each.with_object(Hash.new(0)) { |movie, stat| stat[Date::MONTHNAMES[Date.parse(movie.premierdate).month]] += 1 rescue stat['Undefined'] += 1 }
